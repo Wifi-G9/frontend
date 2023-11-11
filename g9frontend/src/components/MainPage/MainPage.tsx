8cbdcf72fd5c8bccdbd2aabb2e0d2b12fad219e3
Bianca Popu <bianca.popu@gmail.com>
2023-11-11 16:14:50 +0200

    MainPage: Modify layout and style
    Modified the MainPage component so that it fits the entire screen.
    Also modified the MainPage component so that it looks more like the Figma design. Removed the SearchButton, as it was not present in the provided design.
    
    Signed-off-by: Bianca Popu <bianca.popu@gmail.com>

--- g9frontend/src/components/MainPage/MainPage.tsx
+++ g9frontend/src/components/MainPage/MainPage.tsx
@@ -1,68 +1,59 @@
 import React from "react";
 
 import {
-    Container, TextField, Button, ToggleButtonGroup, ToggleButton,
+    Container, TextField, Button, ToggleButtonGroup, ToggleButton, IconButton,
 } from "@mui/material";
 import {Link} from "react-router-dom";
 import "./MainPageStyle.css"
-import SearchBar from "../SearchBar/SearchBar";
 import SearchBarComponent from "../SearchBar/SearchBar";
-
+import { CenterFocusStrong } from "@mui/icons-material";
+import MessageIcon from '@mui/icons-material/Message';
 
 export const MainPage: React.FC = () => {
-
     return(
         <Container>
             <div className="container">
-                <h1 className="header">
-                    <div className="rightContainer1">
-                        <Button style={{alignItems:"flex-end", color:"black", borderColor:"black"}}>
-                            YOUR PROFILE
-
-                        </Button>
-                    </div>
-                </h1>
+                <div className="header">
+                    <p className="logo">WiFi</p>
+                    <Button style={{textTransform: 'none', color:'#f7fefe', right:'3%', fontFamily:'Inter, sans-serif', fontSize:'20px'}}>
+                        Log in
+                    </Button>
+                </div>
                 <div className="content">
                     <div className="leftContainer">
                         {/*here will be the graph part*/}
                     </div>
                     <div className="rightContainer">
-                        <div>
+                        <div className="searchBarContainer">
                             {/*<TextField style={{marginLeft:"8px"}}*/}
                             {/*    id="searchbar"*/}
                             {/*    label="Search a word"*/}
                             {/*    variant="outlined"*/}
                             {/*/>*/}
                             <SearchBarComponent/>
-
-                            <Button type="submit" style={{alignItems:"flex", color:"black", borderColor:"black"}}>
-                                SEARCH
-                            </Button>
                         </div>
-                        <div>
-                            <ToggleButtonGroup style={{backgroundColor:"white"}}
+                        <div className="toggleGroupContainer">
+                            <ToggleButtonGroup style={{backgroundColor:'#e9e9e9', fontFamily:'Inter, sans-serif', width: '100%'}}
                                 color="primary"
                                 exclusive
                                 //onChange={handleChange}
                                 aria-label="Platform"
                             >
-                                <ToggleButton value="instagram">instagram</ToggleButton>
-                                <ToggleButton value="facebook">facebook</ToggleButton>
-                                <ToggleButton value="X">X</ToggleButton>
+                                <ToggleButton className="toggleButton" value='instagram'>Instagram</ToggleButton>
+                                <ToggleButton className="toggleButton" value='facebook'>Facebook</ToggleButton>
+                                <ToggleButton className="toggleButton" value='X'>X</ToggleButton>
                             </ToggleButtonGroup>
+                            <div className="postContentContainer">
+
+                            </div>
                         </div>
 
                     </div>
                 </div>
-                <footer className="footer">
-
-                    <div className="right-container">
-                        <Button style={{color:"white"}}>SEND US A MESSAGE</Button>
-
-                    </div>
-                </footer>
+                <IconButton style={{color:"black", borderRadius: '50%', position: 'fixed', bottom: '20px', right: '20px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)'}}>
+                    <MessageIcon />
+                </IconButton>
             </div>
         </Container>
     )
-
 }
