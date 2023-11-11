accd32055f52b2bff814ac695213cce023607f33
Bianca Popu <bianca.popu@gmail.com>
2023-11-11 16:17:00 +0200

    SearchBar: Add css style
    Signed-off-by: Bianca Popu <bianca.popu@gmail.com>

--- g9frontend/src/components/SearchBar/SearchBar.tsx
+++ g9frontend/src/components/SearchBar/SearchBar.tsx
@@ -1,16 +1,17 @@
-// ButtonComponent.tsx
 import React from 'react';
 import Button from '@mui/material/Button';
+import './SearchBarStyle.css';
 
 const SearchBarComponent: React.FC = () => {
     const [text, setText] = React.useState('');
     return (
         <input
-                    type="text"
-                    placeholder="Search Word"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-            />
+            className='searchBar'
+            type="text"
+            placeholder="Search Word"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+        />
     );
 };
 
