b614a42a2974f72b890b4e25c4858aecae4e0d17
917wiszniewskibianca <91844338+917wiszniewskibianca@users.noreply.github.com>
2023-11-15 19:10:26 +0200

    Update MainPage.tsx
    must run npm install axios
    
    
    Signed-off-by: 917wiszniewskibianca <91844338+917wiszniewskibianca@users.noreply.github.com>

--- g9frontend/src/components/MainPage/MainPage.tsx
+++ g9frontend/src/components/MainPage/MainPage.tsx
@@ -10,6 +10,30 @@ import { CenterFocusStrong } from "@mui/icons-material";
 import MessageIcon from '@mui/icons-material/Message';
 
 export const MainPage: React.FC = () => {
+    const [selectedPlatform, setSelectedPlatform]=  useState();
+    const [posts, setPosts] = useState([]);
+
+    function handleSelect(selectedButton){
+        //selectedButton => 'instagram' , 'facebook' , 'x'
+        setSelectedPlatform(selectedButton);
+    }
+
+    useEffect(() => {
+        if ( selectedPlatform) {
+            const apiUrl = `/search-instagram?query=${selectedPlatform}`;
+
+            axios
+                .get(apiUrl)
+                .then((response) => {
+                    setPosts(response.data["posts-list"]);
+                })
+                .catch((error) => {
+                    console.error("Error fetching Instagram posts:", error);
+                });
+        }
+    }, [selectedPlatform, setPosts]);
+
+    
     return(
         <Container>
             <div className="container">
@@ -33,21 +57,28 @@ export const MainPage: React.FC = () => {
                             <SearchBarComponent/>
                         </div>
                         <div className="toggleGroupContainer">
-                            <ToggleButtonGroup style={{backgroundColor:'#e9e9e9', fontFamily:'Inter, sans-serif', width: '100%'}}
-                                color="primary"
-                                exclusive
-                                //onChange={handleChange}
-                                aria-label="Platform"
-                            >
-                                <ToggleButton className="toggleButton" value='instagram'>Instagram</ToggleButton>
-                                <ToggleButton className="toggleButton" value='facebook'>Facebook</ToggleButton>
-                                <ToggleButton className="toggleButton" value='X'>X</ToggleButton>
-                            </ToggleButtonGroup>
+                              <ToggleButton className="toggleButton" value='instagram' isSelected = {selectedPlatform === "instagram"} onSelect={()=> handleSelect("instagram")}>Instagram</ToggleButton>
+                              <ToggleButton className="toggleButton" value='facebook' isSelected = {selectedPlatform === "facebook"} onSelect={()=> handleSelect("facebook")}>Facebook</ToggleButton>
+                              <ToggleButton className="toggleButton" value='X' isSelected = {selectedPlatform === "x"} onSelect={()=> handleSelect("x")}>X</ToggleButton>
                             <div className="postContentContainer">
-
+                              {!selectedPlatform && <p>Word of the day </p>}
+                              {selectedPlatform  &&
+                                  (posts.map((post) => (
+                                      <div key={post.link}>
+                                          <p>Description: {post.description}</p>
+                                          <p>Likes: {post.likes}</p>
+                                          <p>Comments: {post.comments}</p>
+                                          <p>Link: <a href={post.link} target="_blank" rel="noopener noreferrer">{post.link}</a></p>
+                                          <p>Hashtags: {post.hashtags.join(', ')}</p>
+                                          <p>Engagement Score: {post.engagement_score}</p>
+                                          <p>Sentiment: {post.sentiment_analysis.sentiment}</p>
+                                          <p>Score: {post.sentiment_analysis.score}</p>
+                                          <p>Image Description: {post.image_description}</p>
+                                      </div>
+                                  )))  }
+                
                             </div>
-                        </div>
-
+                      </div>
                     </div>
                 </div>
                 <IconButton style={{color:"black", borderRadius: '50%', position: 'fixed', bottom: '20px', right: '20px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)'}}>
