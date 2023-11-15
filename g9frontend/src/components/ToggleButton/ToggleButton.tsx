8ddbac90b1cbb94d14869ae7953003722e3e50f8
917wiszniewskibianca <91844338+917wiszniewskibianca@users.noreply.github.com>
2023-11-15 19:04:47 +0200

    Update ToggleButton.tsx
    Signed-off-by: 917wiszniewskibianca <91844338+917wiszniewskibianca@users.noreply.github.com>

--- g9frontend/src/components/ToggleButton/ToggleButton.tsx
+++ g9frontend/src/components/ToggleButton/ToggleButton.tsx
@@ -1,1 +1,7 @@
-export {}
+export default function ToggleButton(props){
+    return(
+        <li>
+            <button onClick={props.onSelect}> {props.children} </button>
+        </li>
+    );
+}
