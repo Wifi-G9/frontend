82ae7efd6085ea6d99d34cc4c065a636e01cfb8c
Marti Nicolae-Vlad <martinicolaevlad@gmail.com>
2023-11-17 11:01:46 +0200

    Add Description and SearchBar components
    Signed-off-by: Marti Nicolae-Vlad <martinicolaevlad@gmail.com>

--- g9frontend/src/components/SearchBar/SearchBar.tsx
+++ g9frontend/src/components/SearchBar/SearchBar.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import Button from '@mui/material/Button';
 import './SearchBarStyle.css';
+interface SearchBarProps {
+    onSearch: (searchTerm: string) => void;
+}
 
-const SearchBarComponent: React.FC = () => {
-    const [text, setText] = React.useState('');
+const SearchBarComponent: React.FC<SearchBarProps> = ({onSearch}) => {
+
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+            setSearchTerm(e.target.value);
+        };
+
+    const handleSearch = () => {
+            onSearch(searchTerm);
+        };
+
     return (
-        <input
+        <><input
             className='searchBar'
             type="text"
             placeholder="Search Word"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-        />
+            value={searchTerm}
+            onChange={handleInputChange}/>
+        <Button onClick={handleSearch}>
+            Search
+        </Button></>
     );
 };
 
 export default SearchBarComponent;
+
+
