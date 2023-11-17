c01696c58352b8cecda6779ff9e4fb5136962b3e
917wiszniewskibianca <91844338+917wiszniewskibianca@users.noreply.github.com>
2023-11-17 11:21:03 +0200

    Update GetInsta.tsx
    --updated again
    
    Signed-off-by: 917wiszniewskibianca <91844338+917wiszniewskibianca@users.noreply.github.com>

--- g9frontend/src/components/GetInsta/GetInsta.tsx
+++ g9frontend/src/components/GetInsta/GetInsta.tsx
@@ -1,1 +1,31 @@
-export {}
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useFetchPosts = (theWord, selectedPlatform) => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        let apiUrl = '';
+        if (selectedPlatform === 'instagram') {
+            apiUrl = `/search-instagram?query=${theWord}`;
+        } else if (selectedPlatform === 'facebook') {
+            //apiUrl = `/search-facebook?query=${theWord}`;
+        }
+        else {
+
+        }
+        axios
+            .get(apiUrl)
+            .then((response) => {
+                setPosts(response.data['posts-list']);
+            })
+            .catch((error) => {
+                console.error('Error fetching posts:', error);
+            });
+
+    }, [theWord, selectedPlatform]);
+
+    return posts;
+};
+
+export default useFetchPosts;
