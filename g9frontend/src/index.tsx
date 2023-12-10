5a4522c7be874d97fadf166f865ef5dd2c2d6a3c
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2023-12-10 22:38:28 +0200

    index: Critical error issue solved
    Signed-off-by: Sipos-Lucas-George <lucas.sipos@yahoo.com>

--- g9frontend/src/index.tsx
+++ g9frontend/src/index.tsx
@@ -8,9 +8,15 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  //   FOR SOME FKING REASON IF WE UNCOMMENT THE MAIN PAGE WILL RENDER TWICE
+  //   SO THE BEST SOLUTION WAS TO COMMENT IT... TY FOR YOUR TIME
+  //   KISS LOVE FROM LUCAS & ANDREW
+  //   -- TOOK US LIKE OUR WHOLE SUNDAY AND A BUNCH OF OTHER FILE MODIFICATION
+  //   BUT IN THE END WE FIGURED IT OUT... AT LEAST FOR THE MOMENT --
+  //   AGAIN... KISS LOVE LANDREAS SIPOP <3
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
