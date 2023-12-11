ddbf106c202ad94c2dbf9e8a08aa0a145f6ff8ff
SICOE <sergiu.sicoe@stud.ubbcluj.ro>
2023-12-11 20:44:31 +0200

    craft-post-page 80% done
    Signed-off-by: SICOE <sergiu.sicoe@stud.ubbcluj.ro>

--- g9frontend/src/App.tsx
+++ g9frontend/src/App.tsx
@@ -4,13 +4,16 @@ import './components/MainPage/MainPageStyle.css'
 import {MainPage} from "./components/MainPage/MainPage";
 
 import {BrowserRouter, Routes, Router, Route} from 'react-router-dom';
+import {CraftPost} from "./components/CraftPost/CraftPost";
 
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<MainPage />} />
+                <Route path="/" element={<CraftPost />} />
+                <Route path="/craft-post" element={<CraftPost />} />
+                <Route path="/main" element={<MainPage />} />
 
             </Routes>
         </BrowserRouter>
