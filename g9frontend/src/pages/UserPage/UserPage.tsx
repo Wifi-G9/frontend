c53b797bd1f84606c5dddd11eaa563e0e4c6b511
Andrei-Fabian-Pop <andrei.fabian.pop@gmail.com>
2023-12-13 00:16:10 +0200

    UserPage: added codebase for User Page section
    Signed-off-by: Andrei-Fabian-Pop <andrei.fabian.pop@gmail.com>

---
+++ g9frontend/src/pages/UserPage/UserPage.tsx
@@ -0,0 +1,98 @@
+import React, {useState} from "react";
+import {Button, Container, ToggleButtonGroup} from "@mui/material";
+import ToggleButton from "../../components/ToggleButton/ToggleButton";
+import SendUsAMessageButtonComponent from "../../components/SendUsAMessageButton/SendUsAMessageButton";
+import {useParams} from "react-router-dom";
+import "./UserPageStyle.css";
+
+
+export const UserPage: React.FC = () => {
+    const [selectedPlatform, setSelectedPlatform] = useState<string | null>(null);
+    const {username} = useParams();
+
+    const styles = {
+        blueButton: {
+            width: 300,
+            height: 50,
+            backgroundColor: "#2196f3", /* Blue background color */
+            color: "#ffffff", /* White text color */
+            padding: "10 20",
+            border: "none",
+            borderRadius: 8,
+            fontSize: 16,
+            cursor: "pointer"
+        }
+    }
+
+    const handleSelect = (selectedButton: string): void => {
+        setSelectedPlatform(selectedButton);
+        console.log(username);
+    }
+
+    return (
+        <Container>
+            <div className="container">
+                <div className="header">
+                    <p className="logo">WiFi</p>
+                    <Button style={{
+                        textTransform: 'none',
+                        color: '#f7fefe',
+                        right: '3%',
+                        fontFamily: 'Inter, sans-serif',
+                        fontSize: '20px'
+                    }}>
+                        Log in
+                    </Button>
+                </div>
+                <div className="content">
+                    <div className="leftContainer">
+                        <div className="welcomeText">
+                            Welcome {username}!
+                        </div>
+                        <div className="centeredContainer">
+                            <Button className="blueButton" style={styles.blueButton}>
+                                Craft your post
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="rightContainer">
+                        <div className="toggleGroupContainer">
+                            <ToggleButtonGroup
+                                style={{backgroundColor: '#e9e9e9', fontFamily: 'Inter, sans-serif', width: '100%'}}
+                                color="primary"
+                                exclusive
+                                aria-label="Platform"
+                            >
+                                <ToggleButton className="toggleButton" value='Instagram'
+                                              isSelected={selectedPlatform === "Instagram"}
+                                              onSelect={() => handleSelect("Instagram")}>Instagram</ToggleButton>
+                                <ToggleButton className="toggleButton" value='Facebook'
+                                              isSelected={selectedPlatform === "Facebook"}
+                                              onSelect={() => handleSelect("Facebook")}>Facebook</ToggleButton>
+                                <ToggleButton className="toggleButton" value='X' isSelected={selectedPlatform === "X"}
+                                              onSelect={() => handleSelect("X")}>X</ToggleButton>
+                            </ToggleButtonGroup>
+                            <div className="groupContainerContent">
+                                <Container>
+                                    <div className="centeredContainer"
+                                         style={{display: "flex", flexDirection: "column"}}
+                                    >
+                                        <div className="largeText"
+                                             style={{marginBottom: 20}}
+                                        >
+                                            Please login to {selectedPlatform} to access this content
+                                        </div>
+                                        <Button className="blueButton" style={styles.blueButton}>
+                                            Login to {selectedPlatform}
+                                        </Button>
+                                    </div>
+                                </Container>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <SendUsAMessageButtonComponent/>
+            </div>
+        </Container>
+    );
+}
