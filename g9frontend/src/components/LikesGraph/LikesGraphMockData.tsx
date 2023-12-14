8f313349e0d02730bf872b1f0e0b05c9c063322e
Claudiu <stetcoclaudiu1002@gmail.com>
2023-12-14 14:46:40 +0200

    ToggleButtonContent
    Signed-off-by: Claudiu <stetcoclaudiu1002@gmail.com>

---
+++ g9frontend/src/components/LikesGraph/LikesGraphMockData.tsx
@@ -0,0 +1,142 @@
+interface DataPieceType {
+    date: string,
+    interest: number
+}
+
+const MonthGraphMockData: DataPieceType[] = [
+    {
+        "date": "2020-01-01",
+        "interest": 20
+    },
+    {
+        "date": "2020-02-01",
+        "interest": 30
+    },
+    {
+        "date": "2020-03-01",
+        "interest": 40
+    },
+    {
+        "date": "2020-04-01",
+        "interest": 50
+    },
+    {
+        "date": "2020-05-01",
+        "interest": 60
+    },
+    {
+        "date": "2020-06-01",
+        "interest": 70
+    },
+    {
+        "date": "2020-07-01",
+        "interest": 80
+    },
+    {
+        "date": "2020-08-01",
+        "interest": 90
+    },
+    {
+        "date": "2020-09-01",
+        "interest": 100
+    },
+    {
+        "date": "2020-10-01",
+        "interest": 95
+    },
+    {
+        "date": "2020-11-01",
+        "interest": 90
+    },
+    {
+        "date": "2020-12-01",
+        "interest": 85
+    },
+    {
+        "date": "2021-01-01",
+        "interest": 80
+    },
+    {
+        "date": "2021-02-01",
+        "interest": 75
+    },
+    {
+        "date": "2021-03-01",
+        "interest": 70
+    },
+    {
+        "date": "2021-04-01",
+        "interest": 65
+    },
+    {
+        "date": "2021-05-01",
+        "interest": 60
+    },
+    {
+        "date": "2021-06-01",
+        "interest": 55
+    },
+    {
+        "date": "2021-07-01",
+        "interest": 50
+    },
+    {
+        "date": "2021-08-01",
+        "interest": 65
+    },
+    {
+        "date": "2021-09-01",
+        "interest": 40
+    },
+    {
+        "date": "2021-10-01",
+        "interest": 35
+    },
+    {
+        "date": "2021-11-01",
+        "interest": 30
+    },
+    {
+        "date": "2021-12-01",
+        "interest": 65
+    },
+    {
+        "date": "2022-01-01",
+        "interest": 80
+    },
+    {
+        "date": "2022-02-01",
+        "interest": 15
+    }
+];
+
+
+const YearGraphMockData: DataPieceType[] = [
+    {
+        "date": "2023-01-01",
+        "interest": 88
+    },
+    {
+        "date": "2022-01-01",
+        "interest": 22
+    },
+    {
+        "date": "2021-01-01",
+        "interest": 44
+    },
+    {
+        "date": "2020-01-01",
+        "interest": 55
+    },
+    {
+        "date": "2019-01-01",
+        "interest": 90
+    },
+    {
+        "date": "2018-01-01",
+        "interest": 30
+    }
+];
+
+export {MonthGraphMockData, YearGraphMockData}
+export type {DataPieceType}
